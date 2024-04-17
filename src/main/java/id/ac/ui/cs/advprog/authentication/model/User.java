7077b08976ad0dfe023a2ec91299c9855d39816b
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2024-04-17 20:47:19 +0700

    feat: UserBuilder to build User

--- src/main/java/id/ac/ui/cs/advprog/authentication/model/User.java
+++ src/main/java/id/ac/ui/cs/advprog/authentication/model/User.java
@@ -3,13 +3,24 @@ package id.ac.ui.cs.advprog.authentication.model;
 import lombok.Setter;
 import lombok.Getter;
 
-@Getter @Setter
+@Getter
+@Setter
 public class User {
-    private String fullname;
-    private String email;
-    private String phone;
-    private String password;
-    private String bio;
-    private String gender;
-    private Integer idUser;
+  private String fullname;
+  private String email;
+  private String phone;
+  private String password;
+  private String bio;
+  private String gender;
+  private Integer idUser;
+
+  public User(UserBuilder builder) {
+    this.fullname = builder.fullname;
+    this.email = builder.email;
+    this.phone = builder.phone;
+    this.password = builder.password;
+    this.bio = builder.bio;
+    this.gender = builder.gender;
+    this.idUser = builder.idUser;
+  }
 }
