da85d679f6c47884bb67e5c5dce0a749b328fba0
eryawww <zazaneryawan@gmail.com>
2024-05-08 12:37:02 +0700

    [GREEN] implement editUser controller and service

--- src/main/java/id/ac/ui/cs/advprog/authentication/controllers/UserController.java
+++ src/main/java/id/ac/ui/cs/advprog/authentication/controllers/UserController.java
@@ -1,23 +1,30 @@
 package id.ac.ui.cs.advprog.authentication.controllers;
 
+import id.ac.ui.cs.advprog.authentication.dtos.RegisterUserDto;
+import id.ac.ui.cs.advprog.authentication.models.builder.UserBuilder;
 import id.ac.ui.cs.advprog.authentication.models.entities.UserEntity;
+import id.ac.ui.cs.advprog.authentication.repositories.UserRepository;
 import id.ac.ui.cs.advprog.authentication.services.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequestMapping("/api/user")
 @RestController
 public class UserController {
+  private final PasswordEncoder passwordEncoder;
   private final UserService userService;
+  private final UserBuilder userBuilder;
 
-  public UserController(UserService userService) {
+  public UserController(UserService userService, PasswordEncoder passwordEncoder) {
+    this.userBuilder = new UserBuilder();
     this.userService = userService;
+    this.passwordEncoder = passwordEncoder;
   }
 
   @GetMapping("/me")
@@ -25,6 +32,17 @@ public class UserController {
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
     UserEntity currentUser = (UserEntity) authentication.getPrincipal();
+
+    return ResponseEntity.ok(currentUser);
+  }
+
+  @PostMapping("/edit")
+  public ResponseEntity<UserEntity> editUser(@RequestBody RegisterUserDto input) {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+    UserEntity currentUser = (UserEntity) authentication.getPrincipal();
+    UserEntity newUser = userBuilder.fromRegisterUserDTO(input, passwordEncoder).build();
+    currentUser = userService.editUser(currentUser, newUser);
 
     return ResponseEntity.ok(currentUser);
   }
--- src/main/java/id/ac/ui/cs/advprog/authentication/services/UserService.java
+++ src/main/java/id/ac/ui/cs/advprog/authentication/services/UserService.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.authentication.services;
 
+import id.ac.ui.cs.advprog.authentication.models.builder.UserBuilder;
 import id.ac.ui.cs.advprog.authentication.models.entities.UserEntity;
 import id.ac.ui.cs.advprog.authentication.repositories.UserRepository;
 import org.springframework.stereotype.Service;
@@ -17,9 +18,19 @@ public class UserService {
 
   public List<UserEntity> allUsers() {
     List<UserEntity> users = new ArrayList<>();
-
     userRepository.findAll().forEach(users::add);
 
     return users;
+  }
+
+  public UserEntity editUser(UserEntity user, UserEntity newUser) {
+    user.setFullName(newUser.getFullName());
+    user.setEmail(newUser.getEmail());
+    user.setPassword(newUser.getPassword());
+    user.setGender(newUser.getGender());
+    user.setBio(newUser.getBio());
+    user.setPhone(newUser.getPhone());
+
+    return userRepository.save(user);
   }
 }
